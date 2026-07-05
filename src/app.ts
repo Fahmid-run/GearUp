ff0c1b3662f31f29b4f37db2871cd163fc5f03d8
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-07-05 20:01:15 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cookieParser from "cookie-parser"
-import express from "express"
+import express, { Application } from "express"
 import cors from "cors"
+import { errorHandler } from "./middlewares/globalErrorHandler"
+import AppError from "./utils/appError"
+import httpstatus from "http-status"
 
 
-const app = express()
+
+const app :Application= express()
 
 
 //middlewares 
@@ -13,6 +17,26 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors({
   
 }))
+
+
+// 404 Not found
+
+app.use((req, res, next) => {
+  next(new AppError ("APi Route Not Found",httpstatus.NOT_FOUND))
+})
+
+
+
+app.use(errorHandler);
+
+
+app.get('/test', async (req, res) => {
+  if (true) {
+    throw new AppError('LLduaaaa', 404);
+  }
+  res.send('OK');
+});
+
 
 
 
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,21 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import AppError from '../utils/appError';
+
+// 2. Global Error Handler Middleware
+export const errorHandler: ErrorRequestHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+  const message = err.message || 'Something Went Worng';
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    errDetails: err.errors || null,
+  });
+};
