c9e63ae0930a08de0f69a757234a0114196cd8e9
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-07-11 03:52:14 +0600

    fix : review route

--- src/modules/Reviews/Review.ts
+++ src/modules/Reviews/Review.ts
@@ -1,46 +1,51 @@
-import { Request, Response, Router } from "express";
-import { catchAsync } from "../../utils/catchAsync";
-import { checkExists } from "../../utils/checkExist";
-import { register } from "node:module";
-import { prisma } from "../../lib/prisma";
-import { removeAllListeners } from "node:cluster";
-import { role } from "../../../prisma/generated/prisma/enums";
-import { sendResponse } from "../../utils/sendResponse";
-import httpstatus from "http-status"
-import { auth } from "../../middlewares/auth.middleware";
+import { Request, Response, Router } from 'express';
+import { catchAsync } from '../../utils/catchAsync';
+import { checkExists } from '../../utils/checkExist';
+import { prisma } from '../../lib/prisma';
 
+import { role } from '../../../prisma/generated/prisma/enums';
+import { sendResponse } from '../../utils/sendResponse';
+import httpstatus from 'http-status';
+import { auth } from '../../middlewares/auth.middleware';
+import AppError from '../../utils/appError';
 
-const route = Router()
+const route = Router();
 
+route.post(
+  '/:gearItemId',
+  auth(role.Customer),
+  catchAsync(async (req: Request, res: Response) => {
+    const customerId = req.user?.authorId as string;
+    const gearItemId = req.params?.gearItemId as string;
+    const { review, rating } = req.body;
+    checkExists(prisma.customer, customerId, 'User does not exists');
 
-route.post("/:gearItemId",auth(role.Customer), catchAsync(async (req: Request, res: Response) => {
-  
-  const customerId = req.user?.authorId;
-  const gearItemId= req.params?.gearItemId as string
-  const { review, rating } = req.body;
-  checkExists(prisma.customer, customerId, "User does not exists")
+    const rental = await prisma.rentalOrder.findUnique({
+      where: {
+        customerId,
+      },
+    });
 
-
-  
-
-  const result = await prisma.reviews.create({
-    data: {
-      review,
-      rating,
-      customerId:customerId!,
-      gearItemId
+    if (rental?.rentalStatus !== 'RETURNED') {
+      throw new AppError('You cannot give a review!!', httpstatus.FORBIDDEN);
     }
-  });
 
-  
-  sendResponse(res, {
-    success: true,
-    statusCode: httpstatus.CREATED,
-    message: 'Review Created Successfully',
-    data:result,
-  });
+    const result = await prisma.reviews.create({
+      data: {
+        review,
+        rating,
+        customerId: customerId!,
+        gearItemId,
+      },
+    });
 
-}))
+    sendResponse(res, {
+      success: true,
+      statusCode: httpstatus.CREATED,
+      message: 'Review Created Successfully',
+      data: result,
+    });
+  }),
+);
 
-
-export const reviewRoute= route
+export const reviewRoute = route;
